7c25143b9c9c526c6a90ea5d46b2add3b4037635
Marshall A Ross <MarshallARoss@gmail.com>
2022-07-09 14:49:13 -0700

    setup coin rows and currency formatter

--- CryptoApp/Core/Home/Views/CoinRowView.swift
+++ CryptoApp/Core/Home/Views/CoinRowView.swift
@@ -10,8 +10,37 @@ import SwiftUI
 struct CoinRowView: View {
     
     let coin: Coin
+    let showHoldingColumn: Bool
     
     var body: some View {
+        HStack(spacing: 0.0) {
+                leftColumn
+            Spacer()
+            if showHoldingColumn {
+              centerColumn
+            }
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+struct CoinRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CoinRowView(coin: dev.coin, showHoldingColumn: true)
+                .previewLayout(.sizeThatFits)
+            CoinRowView(coin: dev.coin, showHoldingColumn: true)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
         HStack(spacing: 0) {
             Text("\(coin.rank)")
                 .font(.caption)
@@ -23,20 +52,26 @@ struct CoinRowView: View {
                 .font(.headline)
                 .padding(.leading, 6)
                 .foregroundColor(.theme.accent)
-            Spacer()
-            VStack(alignment: .trailing){
-                Text("\(coin.currentPrice)")
-                    .bold()
-                    .foregroundColor(.theme.accent)
-                Text("\(coin.priceChangePercentage24H ?? 0)%")
-                    .foregroundColor((coin.priceChangePercentage24H ?? 0) >= 0 ? .theme.green : .theme.red)
-            }
         }
     }
-}
-
-struct CoinRow_Previews: PreviewProvider {
-    static var previews: some View {
-        CoinRowView(coin: dev.coin)
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundColor(Color.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing){
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor((coin.priceChangePercentage24H ?? 0) >= 0 ? .theme.green : .theme.red)
+        }
+        .frame(width: UIScreen.main.bounds.width/3, alignment: .trailing)
     }
 }
--- CryptoApp/Extensions/Double.swift
+++ CryptoApp/Extensions/Double.swift
@@ -9,7 +9,37 @@ import Foundation
 
 extension Double {
     
-    ///Convers a double into a currency with 2-6 decimal places
+    
+    ///Converts a double into a currency with 2 decimal places
+    /// ```
+    /// Convert 2345.67 to $2,345.67
+    /// Convert 23.4567 to $23.45
+    /// Convert 0.234567 to $0.23
+    /// ```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        //        formatter.locale = .current // <= this is the default
+        //        formatter.currencyCode = "usd" // <= this is to change the currency | default
+        //        formatter.currencyCode = "$" // <= this is to change symbol | default
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 6
+        return formatter
+    }
+    
+    ///Converts a double into a currency as a string with 2decimal places
+    /// ```
+    /// Convert 2345.67 to "$2,345.67"
+    /// Convert 23.4567 to "$23.46"
+    /// Convert 0.234567 to "$0.23"
+    /// ```
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
+    ///Converts a double into a currency with 2-6 decimal places
     /// ```
     /// Convert 2345.67 to $2,345.67
     /// Convert 23.4567 to $23.4567
@@ -27,7 +57,7 @@ extension Double {
         return formatter
     }
     
-    ///Convers a double into a currency as a string with 2-6 decimal places
+    ///Converts a double into a currency as a string with 2-6 decimal places
     /// ```
     /// Convert 2345.67 to "$2,345.67"
     /// Convert 23.4567 to "$23.4567"
@@ -38,4 +68,19 @@ extension Double {
         return currencyFormatter6.string(from: number) ?? "$0.00"
     }
     
+    ///Converts a double into string representation with 2 decimal places
+    /// ```
+    /// Convert 1.2345 to "1.23"
+    /// ```
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    ///Converts a double into string representation with 2 decimal places and a % symbol
+    /// ```
+    /// Convert 1.2345 to "1.23%"
+    /// ```
+    func asPercentString() -> String {
+        return asNumberString() + "%"
+    }
 }
