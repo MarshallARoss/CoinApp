9eee6a7d283c23f1656fb30b051c110977797baf
Marshall A Ross <MarshallARoss@gmail.com>
2022-07-22 18:52:11 -0700

    added Network Layer

--- CryptoApp/Models/CoinModel.swift
+++ CryptoApp/Models/CoinModel.swift
@@ -5,6 +5,7 @@
 //  Created by Marshall  on 6/17/22.
 //
 
+//Demo Coin
 /*
  API Info
  URL: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
